4a61e966791d0d455689784803a137524fa6064a
nguyenlc12 <nguyenlc@zigexn.vn>
2024-09-11 17:40:18 +0700

    Test rubocop file

---
+++ test.rb
@@ -0,0 +1,27 @@
+# Named arguments
+def print_info name:, age:, address: "HCM"
+  puts "Name: #{name}. Age: #{age}. Address: #{address}."
+end
+
+print_info(age: 28, name: "Nguyen", address: "DN")
+print_info(age: 28, name: "Nguyen")
+
+# Splat arguments
+def print_list_names *name
+  p name
+end
+
+print_list_names
+print_list_names("Nguyen", "Cris", "David")
+
+# Keyword-based splat arguments
+def print_options **option
+  p option
+end
+
+print_options
+print_options(name: "Nguyen", age: 29)
+
+def is_even?(int_number)
+  int_number % 2 == 0
+end
